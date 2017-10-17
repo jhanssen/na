e2cad3599c206166f513feb5ae3881678136fb24
Jan Erik Hanssen <jhanssen@gmail.com>
2017-10-17 13:37:48 -0700

    stuff

---
+++ index.js
@@ -0,0 +1,12 @@
+/*global require,process*/
+
+const daemon = require("./lib/daemon");
+const argv = require("minimist")(process.argv.slice(2));
+
+daemon.send(process.cwd(), argv).then(ret => {
+    console.log(ret);
+    process.exit();
+}).catch(e => {
+    console.error(e);
+    process.exit();
+});
