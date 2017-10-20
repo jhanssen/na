d6c12b296bd5e9de6fc533d738490458311749a3
Jan Erik Hanssen <jhanssen@gmail.com>
2017-10-20 01:29:21 -0700

    Allow running in foreground for debugging purposes

--- index.js
+++ index.js
@@ -5,10 +5,14 @@
 const daemon = require("./lib/daemon");
 const argv = require("minimist")(process.argv.slice(2));
 
-daemon.send(process.cwd(), argv).then(ret => {
-    console.log(ret);
-    process.exit();
-}).catch(e => {
-    console.error(e);
-    process.exit();
-});
+if (argv.daemon === false) {
+    daemon.run(argv);
+} else {
+    daemon.send(process.cwd(), argv).then(ret => {
+        console.log(ret);
+        process.exit();
+    }).catch(e => {
+        console.error(e);
+        process.exit();
+    });
+}
